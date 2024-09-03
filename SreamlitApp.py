83cfb4dabaff23d3e63b59aaac8a7435ca46798a
Manoj Kr Yadav <113343587+yadavmanojkr@users.noreply.github.com>
2024-09-03 20:19:04 +0530

    Update SreamlitApp.py

--- SreamlitApp.py
+++ SreamlitApp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_helper import get_few_shot_db_chain
 
-st.title("AtliQ T Shirts: Database Q&A 👕")
+st.title("Max T Shirts: Database Q&A 👕")
 
 question = st.text_input("Question: ")
 
